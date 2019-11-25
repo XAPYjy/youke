f18842503dc5313362b0c468f0dad1271daa737b
xiaobbear <xiaobbear@163.com>
2019-11-25 11:27:21 +0800

    三次更新

--- youke/urls.py
+++ youke/urls.py
@@ -1,31 +1,10 @@
-<<<<<<< HEAD
-from django.urls import path, include
-
-urlpatterns = [
-    path(r'', include('back_system.urls', namespace='bk')),
-=======
-"""youke URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-
 from ykuser.urls import router_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', include('back_system.urls', namespace='bk')),
     path('youke/', include(router_user.urls))
->>>>>>> 229a5112c2263ab22daeaae144d896d46ecd0558
+
 ]
