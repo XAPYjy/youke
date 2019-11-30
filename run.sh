455547304398c3749a5d97d5eb22e375fc2d8502
cys <724553598@qq.com>
2019-11-30 17:15:06 +0800

    更新

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo 'staring project'
 cd /usr/src/youke
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
 cd /usr/src/youke
-gunicorn -w 1 -b 0.0.0.0:8888 youke.wsgi:application
+python manage runserver 47.92.132.161
