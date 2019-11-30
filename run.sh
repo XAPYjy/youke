f12005f68d94a6cda575d676ca1450a4157ab71b
cys <724553598@qq.com>
2019-12-01 00:09:41 +0800

    更新了run.sh文件

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ echo 'staring project'
 cd /usr/src/youke
 pip install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
 cd /usr/src/youke
-python manage runserver 47.92.132.161
+# gunicorn -w 1 -b 0.0.0.0:8888 youke.wsgi:application
+python manage runserver 0.0.0.0:8888
